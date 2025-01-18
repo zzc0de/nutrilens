d4b43fa53fedde3fab2003899464584a98d7fdb6
Azed Muhammad Noval <azedmuhammadn@gmail.com>
2025-01-18 20:20:35 +0700

    Refactor Home component to integrate HeroSection and BenefitsList for improved layout and content presentation

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,21 @@
 import Button from "../../components/atoms/Button/Button";
 import Container from "../../components/atoms/Container/Container";
+import BenefitsList from "../../components/molecules/BenefitsList";
+import HeroSection from "../../components/molecules/HeroSection";
 
 const Home = () => {
   return (
     <>
-      <Container className={"h-screen flex justify-center items-center"}>
-        <div className="w-1/2 h-full flex flex-col justify-center items-start gap-y-4">
-          <h1 className="text-5xl font-bold">Kenali Kalori Makanan Anda</h1>
-          <p>
-            Dengan aplikasi ini, Anda bisa mengetahui kandungan kalori dan
-            nutrisi setiap hidangan hanya dengan memindainya. Dilengkapi dengan
-            teknologi AI canggih.
-          </p>
-          <Button className={"bg-primary-blue text-white"}>Get Started</Button>
-        </div>
-        <div className="w-1/2 h-full items-center justify-center flex">
-          <img
-            src="/src/assets/health.svg"
-            alt="hero"
-            className="object-cover h-96"
-          />
-        </div>
-      </Container>
-      <Container className={"flex flex-col"}>
+      <HeroSection
+        title={"Kenali Kalori Makanan Anda"}
+        desc={
+          "Dengan aplikasi ini, Anda bisa mengetahui kandungan kalori dan nutrisi setiap hidangan hanya dengan memindainya. Dilengkapi dengan teknologi AI canggih."
+        }
+      />
+      <Container className={"flex flex-col gap-y-6"}>
         <div className="w-full p-6 text-center">
-          <h1 className="text-3xl font-extrabold underline underline-offset-8">
-            Kenapa harus NutriLens?
+          <h1 className="text-3xl font-extrabold">
+            Kenapa harus <span className="text-orange-400">Nutrilens</span>?
           </h1>
         </div>
         <div className="flex w-full">
@@ -46,6 +36,27 @@ const Home = () => {
           </div>
         </div>
       </Container>
+      <div className="w-full bg-primary-blue p-10 gap-y-6 flex flex-col">
+        <h1 className="text-center font-bold text-white text-4xl font-Inter">
+          Manfaat Nutrilens
+        </h1>
+        <BenefitsList
+          benefits={[
+            {
+              title: "Cepat",
+              desc: "Dapatkan informasi nutrisi dalam hitungan detik.",
+            },
+            {
+              title: "Tepat",
+              desc: "Dapatkan informasi nutrisi yang akurat.",
+            },
+            {
+              title: "Mudah",
+              desc: "Pindai makanan Anda dan dapatkan informasi nutrisi.",
+            },
+          ]}
+        />
+      </div>
     </>
   );
 };
